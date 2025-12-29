8f2f0bf361f37157389392faa0f962324b67281e
hawoot <hamrit.nidhal@gmail.com>
2025-12-29 22:59:52 +0000

    feat: Add vite-plugin-singlefile for single file output in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // Vite configuration - tells Vite how to build your app
 // Similar to a webpack.config.js or a setup.cfg in Python
 export default defineConfig({
-  plugins: [react()],  // Enable React/JSX support
+  plugins: [react(), viteSingleFile()],  // Enable React/JSX support + single file output
 })

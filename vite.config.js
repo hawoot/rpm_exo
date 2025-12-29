db1a92f60411171d84ca2be35463969a60103662
hawoot <hamrit.nidhal@gmail.com>
2025-12-29 23:33:03 +0000

    Revert "feat: Add vite-plugin-singlefile for single file output in Vite configuration"
    This reverts commit 8f2f0bf361f37157389392faa0f962324b67281e.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // Vite configuration - tells Vite how to build your app
 // Similar to a webpack.config.js or a setup.cfg in Python
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],  // Enable React/JSX support + single file output
+  plugins: [react()],  // Enable React/JSX support
 })
